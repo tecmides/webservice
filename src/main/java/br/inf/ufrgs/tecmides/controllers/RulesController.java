056a6b7df6d31b40acd28381b6f339d82aa0ceb6
Luiz Henrique Longhi Rossi <lh.rossi@gmail.com>
2018-08-09 13:22:00 -0300

    configuração inicial JPA

--- src/main/java/br/inf/ufrgs/tecmides/controllers/RulesController.java
+++ src/main/java/br/inf/ufrgs/tecmides/controllers/RulesController.java
@@ -5,11 +5,13 @@
  */
 package br.inf.ufrgs.tecmides.controllers;
 
-import br.inf.ufrgs.tecmides.services.RulesService;
 import br.inf.ufrgs.tecmides.entities.Rule;
+import br.inf.ufrgs.tecmides.services.RulesService;
 import java.security.Principal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,25 +23,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rules")
 @RestController
 public class RulesController {
-    
+
     @Autowired
     RulesService rulesService;
-    
+
     @RequestMapping(value = "/generate", method = RequestMethod.GET)
     public List<Rule> generate(Principal principal) {
-        
+
         String ARFFString = "";
-        
+        System.out.println("teste");
         return rulesService.generateRules(ARFFString, 0, 0, 0);
     }
-    
+
     @RequestMapping(value = "/generateByAttrRelativity", method = RequestMethod.GET)
     public List<Rule> generateByAttrRelativity(Principal principal) {
-        
-         String ARFFString = "";
-         
+
+        String ARFFString = "";
+
 //         String ARFFString, int idxClassAttr, int numRules, double minSupport, double minConfidence
         return rulesService.generateRulesByAttrRelativity(ARFFString, 0, 0, 0, 0);
+    }
+
+    @RequestMapping(value = "/createTrainingSet", method = RequestMethod.POST) 
+    public ResponseEntity createTraingSet (@RequestBody List<Integer> trainingSet){
+        
+        return ResponseEntity.ok("OK");
     }
     
 }
---
+++ src/main/java/br/inf/ufrgs/tecmides/services/InteractionService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.inf.ufrgs.tecmides.services;
+
+import br.inf.ufrgs.tecmides.repositories.InteractionRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author preto
+ */
+@Service
+public class InteractionService {
+    
+    InteractionRepository repository;
+    public void saveAll(List<Integer> trainingSet){
+        
+        for (Integer i: trainingSet){
+            repository.save(i);
+        }
+        
+    }
+}
